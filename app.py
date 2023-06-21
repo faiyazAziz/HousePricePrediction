773ee958664fab32ad93ea55425197eed30098b0
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2023-06-21 10:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,26 @@ def home():
 @app.route('/predict',methods = ['POST'])
 def predict():
 
-    id = request.form.get('id')
-    return jsonify({'id':str(id)})
-    # area = request.form.get('area')
-    # noOfBedroom = request.form.get('noOfBedroom')
-    # noOfBathroom = request.form.get('noOfBathroom')
-    # noOfStories = request.form.get('noOfStories')
-    # mainRoad = request.form.get('mainRoad')
-    # guestRoom = request.form.get('guestRoom')
-    # basement = request.form.get('basement')
-    # hotWaterHeating = request.form.get('hotWaterHeating')
-    # airConditioning = request.form.get('airConditioning')
-    # parking = request.form.get('parking')
-    # prefarea = request.form.get('prefarea')
-    # furnishingstatus = request.form.get('furnishingstatus')
+    area = request.form.get('area')
+    noOfBedroom = request.form.get('noOfBedroom')
+    noOfBathroom = request.form.get('noOfBathroom')
+    noOfStories = request.form.get('noOfStories')
+    mainRoad = request.form.get('mainRoad')
+    guestRoom = request.form.get('guestRoom')
+    basement = request.form.get('basement')
+    hotWaterHeating = request.form.get('hotWaterHeating')
+    airConditioning = request.form.get('airConditioning')
+    parking = request.form.get('parking')
+    prefarea = request.form.get('prefarea')
+    furnishingstatus = request.form.get('furnishingstatus')
 
-    # input_data = np.array([[ area, noOfBedroom, noOfBathroom,
-    #                         noOfStories, mainRoad,guestRoom, basement,
-    #                         hotWaterHeating, airConditioning,parking,
-    #                         prefarea, furnishingstatus]])
-    # result = model.predict(input_data)[0]
+    input_data = np.array([[ area, noOfBedroom, noOfBathroom,
+                            noOfStories, mainRoad,guestRoom, basement,
+                            hotWaterHeating, airConditioning,parking,
+                            prefarea, furnishingstatus]])
+    result = model.predict(input_data)[0]
 
-    # return jsonify({'price':str(result)})
+    return jsonify({'price':str(result)})
 
 
 if __name__ == '__main__':
